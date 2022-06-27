2e002b03658f8c7490e2ef040ff638133e68db5b
Ana Valdemoro <ana_v_r97@hotmail.com>
2022-06-27 11:34:43 +0100

    CLP: add equals days steps to fixture

--- src/dataStepsFixture.ts
+++ src/dataStepsFixture.ts
@@ -9,3 +9,13 @@ export const randomDaysSteps = (maximunStepsPerDay: number[]): number[][] => {
     });
 
 };
+
+const monday: number[] = [5448, 8041, 6573, 8104];
+const tuesday: number[] = [5448, 8041, 6573, 8104];
+const wednesday: number[] = [5448, 8041, 6573, 8104];
+const thursday: number[] = [5448, 8041, 6573, 8104];
+const friday: number[] = [5448, 8041, 6573, 8104];
+const saturday: number[] = [5448, 8041, 6573, 8104];
+const sunday: number[] = [5448, 8041, 6573, 8104];
+
+export const daysOfEqualSteps: number[][] = [monday, tuesday, wednesday, thursday, friday, saturday, sunday];
